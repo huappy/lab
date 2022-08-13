13d14aa76be04c9701f9857967acb87c63216406
huappy <111156347+huappy@users.noreply.github.com>
2022-08-13 14:38:47 -0500

    Classes.py from lab 9
    Classes.py from lab 9 now to be given test docs and docstrings for lab 12

---
+++ classes.py
@@ -0,0 +1,68 @@
+class Television:
+    MIN_channel = 0     # Minimum TV __channel
+    MAX_channel = 3     # Maximum TV __channel
+
+    MIN_volume = 0      # Minimum TV __volume
+    MAX_volume = 2      # Maximum TV __volume
+
+    def __init__(self):
+
+        self.__channel = Television.MIN_channel
+        self.__volume = Television.MIN_volume
+        self.__on = "False"
+
+        pass
+
+    def power(self):
+        if self.__on =="False":
+            self.__on = "True"
+        else:
+            self.__on = "False"
+
+        pass
+
+    def channel_up(self):
+        if self.__on == "True":
+            if self.__channel == Television.MAX_channel:
+                self.__channel = Television.MIN_channel
+            else:
+                self.__channel += 1
+        else:
+            pass
+        
+        pass
+
+    def channel_down(self):
+        if self.__on == "True":
+            if self.__channel == Television.MIN_channel:
+                self.__channel = Television.MAX_channel
+            else:
+                self.__channel -= 1
+        else:
+            pass
+
+        pass
+
+    def volume_up(self):
+        if self.__on == "True":
+            if self.__volume == Television.MAX_volume:
+                pass
+            else:
+                self.__volume += 1
+
+        pass
+
+    def volume_down(self):
+        if self.__on == "True":
+            if self.__volume == Television.MIN_volume:
+                pass
+            else:
+                self.__volume -= 1
+
+        pass
+
+    def __str__(self):
+        
+        return f"TV status: Is on = {self.__on}, channel = {self.__channel}, volume = {self.__volume}"
+
+        pass
