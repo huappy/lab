82cd3549e8675a3bbf03ccee6a1c08d64ab05d49
huappy <111156347+huappy@users.noreply.github.com>
2022-08-13 15:22:35 -0500

    classes.py Docstrings added
    Added docstrings to classes.py

--- classes.py
+++ classes.py
@@ -1,4 +1,10 @@
+
+
 class Television:
+    '''
+    This class defines an instance of a Television and its settings
+    including its power(on or off), the current channel, and the current volume.
+    '''
     MIN_channel: int = 0     # Minimum TV __channel
     MAX_channel: int = 3     # Maximum TV __channel
 
@@ -6,7 +12,11 @@ class Television:
     MAX_volume: int = 2      # Maximum TV __volume
 
     def __init__(self):
-
+        '''
+        Defines the initial settngs of the Television
+        
+        :param self: This television's current settings.
+        '''
         self.__channel: int = Television.MIN_channel
         self.__volume: int = Television.MIN_volume
         self.__on: bool = "False"
@@ -14,6 +24,11 @@ class Television:
         pass
 
     def power(self):
+        '''
+        Acts as a power button. If the TV is off, the power button turns it on and vice versa
+        
+        :param self: This television's current settings.
+        '''
         if self.__on =="False":
             self.__on = "True"
         else:
@@ -22,6 +37,13 @@ class Television:
         pass
 
     def channel_up(self):
+        '''
+        Adds one to the channel based on what the channel is currently set to. 
+        For example if the channel is currently at 1, calling the function would set it to 2
+        If the channel is already at its maximum, it will return the channel to its minimum.
+        
+        :param self: This television's current settings.
+        '''
         if self.__on == "True":
             if self.__channel == Television.MAX_channel:
                 self.__channel = Television.MIN_channel
@@ -33,6 +55,13 @@ class Television:
         pass
 
     def channel_down(self):
+        '''
+        Subtracts one from the channel based on what the channel is currently set to. 
+        For example if the channel is currently at 2, calling the function would set it to 1
+        If the channel is already at its minimum, it will return the channel to its maximum.
+        
+        :param self: This television's current settings.
+        '''
         if self.__on == "True":
             if self.__channel == Television.MIN_channel:
                 self.__channel = Television.MAX_channel
@@ -44,6 +73,12 @@ class Television:
         pass
 
     def volume_up(self):
+        '''Adds one to the volume based on what the volume is currently set to. 
+        For example if the volume is currently at 1, calling the function would set it to 2
+        If the volume is already at its maximum, the function will not chanve the volume.
+        
+        :param self: This television's current settings.
+        '''
         if self.__on == "True":
             if self.__volume == Television.MAX_volume:
                 pass
@@ -53,6 +88,13 @@ class Television:
         pass
 
     def volume_down(self):
+        '''
+        Subtracts one from the volume based on what the volume is currently set to. 
+        For example if the volume is currently at 2, calling the function would set it to 1
+        If the volume is already at its minimum, the function will not chanve the volume.
+        
+        :param self: This television's current settings.
+        '''
         if self.__on == "True":
             if self.__volume == Television.MIN_volume:
                 pass
@@ -62,7 +104,12 @@ class Television:
         pass
 
     def __str__(self):
+        '''
+        Gives the current status of the TV including power, channel, and volume.
         
+        :param self: This television's current settings
+        :return: The status of the TV.
+        '''
         return f"TV status: Is on = {self.__on}, channel = {self.__channel}, volume = {self.__volume}"
 
         pass
